e8b3637f43211624b2b1f61cb4fa742142cdb90b
Zakaria Moumni <zmoumni@e1r9p9.1337.ma>
2025-07-04 11:26:02 +0100

    update: enhance Docker setup and configuration, add .env file, and remove sensitive data

--- Makefile
+++ Makefile
@@ -1,6 +1,15 @@
-all :
-        cd srcs && docker-compose up --build
-down :
-        cd srcs && docker-compose down --rmi all
+all:
+	@echo "\033[0;32m🚀 Building and starting Inception containers...\033[0m"
+	cd srcs && docker compose up --build
 
-re :    down all
+# Stop and remove all containers, volumes, and images
+down:
+	@echo "\033[0;31m🧹 Stopping and removing everything...\033[0m"
+	cd srcs && docker compose down --rmi all --volumes
+
+# Rebuild: full clean and then launch again
+re: down all
+
+# Optional: show container status
+status:
+	cd srcs && docker compose ps
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -8,15 +8,12 @@ chmod +x wp-cli.phar
 
 mv wp-cli.phar /usr/wp
 
-# cp wp-config-sample.php wp-config.php
-
-
 /usr/wp core download --allow-root
-/usr/wp config create --dbname=wordpress --dbuser=zmoumni --dbpass=password --dbhost=mariadb --allow-root
-/usr/wp core install --url=localhost --title=inception --admin_user=zmoumni --admin_password=password --admin_email=zmoumni@admin.com --allow-root
- 
 
-/usr/wp user create zakaria zakaria@example.com --role=author --user_pass=1234 --allow-root
+/usr/wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_HOST --allow-root
 
+/usr/wp core install --url=$DOMAINE_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --allow-root
+
+/usr/wp user create $WP_AUTHOR_USER $WP_AUTHOR_EMAIL --role=author --user_pass=$WP_AUTHOR_PASSWORD --allow-root
 
 php-fpm7.4 -F
