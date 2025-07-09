e2660b78a7bb3d02d8f4ac6fac37e8104d9d7844
MoumniZakaria <iamzakariamoumni.com>
2025-07-09 13:17:57 +0100

    updating

--- srcs/requirements/mariadb/scripts/setup.sh
+++ srcs/requirements/mariadb/scripts/setup.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+
+
 # Start MariaDB service
 service mariadb start
+
+while ! mysqladmin ping --silent; do
+    sleep 1
+    echo "waiting for mariadb"
+done
+
 
 # Create database if not exists
 mysql -u root -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
@@ -9,11 +17,18 @@ mysql -u root -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
 # Create user if not exists  
 mysql -u root -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
 
+# mysql -u root -e "CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY 'root1234';"
+
 # Grant privileges
 mysql -u root -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;"
+
 mysql -u root -e "FLUSH PRIVILEGES;"
 
-# Stop service and start in foreground
-service mariadb stop
 
-exec mysqld_safe --user=mysql
+# mysqladmin -u root -p"root1234" shutdown
+
+# Stop service and start in foreground
+# service mariadb stop
+
+exec mysqld_safe
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
-
 sleep 10
 
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-
-chmod +x wp-cli.phar
-
-mv wp-cli.phar /usr/wp
-
-# cp wp-config-sample.php wp-config.php
+cd /var/www/html
 
 
-/usr/wp core download --allow-root
-/usr/wp config create --dbname=wordpress --dbuser=zmoumni --dbpass=password --dbhost=mariadb --allow-root
-/usr/wp core install --url=localhost --title=inception --admin_user=zmoumni --admin_password=password --admin_email=zmoumni@admin.com --allow-root
- 
+# curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+# chmod +x wp-cli.phar
+# mv wp-cli.phar /usr/wp
 
-/usr/wp user create zakaria zakaria@example.com --role=author --user_pass=1234 --allow-root
+if ! /usr/wp core is-installed --allow-root --path="/var/www/html"; then
+    /usr/wp core download --allow-root --path="/var/www/html"
+    /usr/wp config create --dbname="$MYSQL_DATABASE" --dbuser="$MYSQL_USER" --dbpass="$MYSQL_PASSWORD" --dbhost="$WORDPRESS_DB_HOST" --allow-root --path="/var/www/html"
+    /usr/wp core install --url="$DOMAINE_NAME" --title="$WORDPRESS_TITLE" --admin_user="$WORDPRESS_ADMIN_USER" --admin_password="$WORDPRESS_ADMIN_PASSWORD" --admin_email="$WORDPRESS_ADMIN_EMAIL" --allow-root --path="/var/www/html"
+fi
 
+if ! /usr/wp user get "$WORDPRESS_AUTHOR_USER" --allow-root --path="/var/www/html" > /dev/null 2>&1; then
+    /usr/wp user create "$WORDPRESS_AUTHOR_USER" "$WORDPRESS_AUTHOR_EMAIL" --role=author --user_pass="$WORDPRESS_AUTHOR_PASSWORD" --allow-root --path="/var/www/html"
+fi
 
 php-fpm7.4 -F
